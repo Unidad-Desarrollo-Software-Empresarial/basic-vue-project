a29caa1a3be3d99a7712654fdc320b6c4b5f9ee4
Cristian Paredes <csparedes@pucesi.edu.ec>
2024-05-29 16:31:01 -0500

    iniciado tabla dinamica

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <p>App.vue</p>
+  <CustomDataTableUi />
 </template>
 
-<script setup>
+<script setup lang="ts">
+import CustomDataTableUi from './components/ui/CustomDataTableUi.vue'
 </script>
---
+++ src/components/ui/CustomDataTableUi.vue
@@ -0,0 +1,169 @@
+<template>
+    <div>
+        <div class="p-4 bg-white dark:bg-gray-900">
+            <label for="table-search" class="sr-only">Search</label>
+            <div class="relative mt-1">
+                <div class="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
+                    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                    </svg>
+                </div>
+                <input type="text" id="table-search"
+                    class="block pt-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Buscar..." />
+            </div>
+        </div>
+    </div>
+    <div>
+        <fwb-dropdown text="Columnas" class="ml-4 pr-2">
+            <div class="min-w-[300px] max-h-[400px] overflow-auto pl-4">
+                <!-- <p :class="[`p-2 hover:bg-slate-400 hover:rounded-lg  hover:text-white`]" v-for="header in columns" :key="`${header.isShowing}${header.head}`">
+                    {{ header.head }}
+                    <span v-if="header.isShowing" class="text-xs text-slate-400 ">Mostrando</span>
+                </p> -->
+                <div v-for="(header, index) in columns" :key="index" :class="[
+                    `p-2 hover:bg-slate-400 hover:rounded-lg  hover:text-white`,
+                    `flex justify-between`
+                ]" @click="onChangeVisibilityColumn(index)">
+                    <p>{{ header.head }}</p>
+                    <p class="px-4">{{ header.isShowing }}</p>
+                </div>
+            </div>
+        </fwb-dropdown>
+    </div>
+    <div>
+        <fwb-table hoverable>
+            <fwb-table-head>
+                <!-- <div v-if="isFetching"></div> -->
+                <fwb-table-head-cell v-for="head in columns" :key="head.head">
+                    <div v-if="head.isShowing">
+                        {{ head.head }}
+                    </div>
+                </fwb-table-head-cell>
+            </fwb-table-head>
+            <!-- Body -->
+            <fwb-table-body>
+                <fwb-table-row v-for="row in rows" :key="row?.INTER_ID">
+                    <fwb-table-cell v-for="(cell, cellIndex) in Object.values(row ? row : [])" :key="cell">
+                        <div v-show="columns[cellIndex].isShowing">
+                            {{ cell }}
+                        </div>
+                    </fwb-table-cell>
+                </fwb-table-row>
+            </fwb-table-body>
+        </fwb-table>
+    </div>
+    <div>
+        <nav class="flex items-center flex-column flex-wrap md:flex-row justify-between p-4"
+            aria-label="Table navigation">
+            <span
+                class="text-sm font-normal text-gray-500 dark:text-gray-400 mb-4 md:mb-0 block w-full md:inline md:w-auto">Mostrando
+                <span class="font-semibold text-gray-900 dark:text-white">1-10</span> de
+                <span class="font-semibold text-gray-900 dark:text-white">1000</span></span>
+            <ul class="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
+                <li>
+                    <a href="#"
+                        class="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Anterior</a>
+                </li>
+                <li>
+                    <a href="#"
+                        class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">1</a>
+                </li>
+                <li>
+                    <a href="#"
+                        class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
+                </li>
+                <li>
+                    <a href="#"
+                        class="flex items-center justify-center px-3 h-8 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</a>
+                </li>
+                <li>
+                    <a href="#"
+                        class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">4</a>
+                </li>
+                <li>
+                    <a href="#"
+                        class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">5</a>
+                </li>
+                <li>
+                    <a href="#"
+                        class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Siguiente</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { useQuery } from '@tanstack/vue-query'
+import axios from 'axios'
+import {
+    // FwbA,
+    FwbTable,
+    FwbTableBody,
+    FwbTableCell,
+    FwbTableHead,
+    FwbTableHeadCell,
+    FwbTableRow,
+    FwbDropdown
+    // FwbButton
+} from 'flowbite-vue'
+import type { MatrizResponseInterface } from '../interfaces/matriz.interface'
+import { computed, onMounted, ref, watch } from 'vue'
+
+const { data } = useQuery({
+    queryKey: ['matriz'],
+    queryFn: async () => {
+        const response = await axios.get<MatrizResponseInterface[]>(
+            'https://gestiondocumental.pucesi.edu.ec/index.php/api/matriz'
+        )
+        return response.data
+    }
+})
+
+const columns = ref<{ head: string; isShowing: boolean }[]>([])
+
+watch(data, () => {
+    if (data.value) {
+        columns.value = Object.keys(data.value[0]).map((col) => {
+            return { head: col, isShowing: true }
+        })
+    }
+})
+
+onMounted(() => {
+    if (data.value) {
+        columns.value = Object.keys(data.value[0]).map((col) => {
+            return { head: col, isShowing: true }
+        })
+    }
+})
+
+const rowsPerPage = ref<number>(3)
+const currentPage = ref<number>(1)
+const totalPages = computed(() => {
+    return Math.ceil(data.value ? data.value.length / rowsPerPage.value : 0)
+})
+
+const rows = computed(() => {
+    console.log({ currentPage, totalPages })
+    const rowsToShow = data.value ? data.value : []
+    return rowsToShow.map((row, index) => {
+        if (
+            index >= (currentPage.value - 1) * rowsPerPage.value &&
+            index < currentPage.value * rowsPerPage.value
+        ) {
+            return row
+        }
+    })
+})
+
+const onChangeVisibilityColumn = (index: number) => {
+    columns.value[index].isShowing = !columns.value[index].isShowing
+}
+
+</script>
+
+<style scoped></style>
